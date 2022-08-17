e19395633b69ddd32132dffad1fc2d2421995b37
Kevin Owens <33699674+kevikodes@users.noreply.github.com>
2022-08-17 04:48:08 -0700

    remove code in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App";
-import { TransactionsProvider } from "./context/TransactionContext";
-import "./styles/global.css";
+import App from './App'
+import './styles/global.css'
 
 ReactDOM.render(
-  <TransactionsProvider>
-    <App />
-  </TransactionsProvider>,
-  document.getElementById("root")
-);
+  <App />,
+
+  document.getElementById('root'),
+)
